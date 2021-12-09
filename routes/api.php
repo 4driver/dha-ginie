f70f8b099e7518ab04bfc5fb91229ed5d865c67b
Moiz Chauhdry <40860743+moizchauhdry@users.noreply.github.com>
2021-12-09 11:25:12 +0500

    Login & Register API

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| WEB & MOBILE FRONTEND ROUTES
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
+*/
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
+
+/*
+|--------------------------------------------------------------------------
+| BACKEND ADMIN DASHBOARD ROUTES
+|--------------------------------------------------------------------------
 */
 
 Route::get('version', function () {
